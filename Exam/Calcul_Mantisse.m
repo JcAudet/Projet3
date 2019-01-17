e4fec41e876698a9da77578131c490aa43b670d1
JcAudet <imflyingtoast@hotmail.com>
2019-01-17 17:54:08 -0500

    Code pour convertir en binaire

---
+++ Exam/Calcul_Mantisse.m
@@ -0,0 +1,21 @@
+clear all;
+clc;
+
+Mant=0.4580078125;
+Bin=[];
+i=1;
+
+while Mant ~= 1
+    Mant=Mant*2;
+    if Mant>1
+        Mant=Mant-1;
+        Bin=[1 Bin];
+    elseif Mant<1
+        Bin=[0 Bin];
+    elseif Mant==1
+        Bin=[0 Bin];
+    end
+    i=i+1;
+end
+
+Bin
