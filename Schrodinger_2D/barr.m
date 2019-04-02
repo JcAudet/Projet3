fb463872c252192175b741557a2698d50815462e
JcAudet <imflyingtoast@hotmail.com>
2019-04-02 12:08:36 -0400

    Potentiels

--- Schrodinger_2D/barr.m
+++ Schrodinger_2D/barr.m
@@ -1,4 +1,4 @@
-function barr=barr(x,y,V,xm,h,yf1,yf2,a)
+function barr=barr(x,y,V,xm,h,yf1,yf2,a,type)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Variables
@@ -17,40 +17,45 @@ function barr=barr(x,y,V,xm,h,yf1,yf2,a)
 
 barr = zeros(length(y),length(x));
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %% Carree
-% 
-% X = x>(xm-h/2) & x<(xm+h/2);
-% Y = y>(yf1-a/2) & y<(yf1+a/2) | y>(yf2-a/2) & y<(yf2+a/2);
-% 
-% barr(~Y,X)=V;
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Carree
+
+if strcmp(type,'Carre')
+
+    X = x>(xm-h/2) & x<(xm+h/2);
+    Y = y>(yf1-a/2) & y<(yf1+a/2) | y>(yf2-a/2) & y<(yf2+a/2);
+
+    barr(~Y,X)=V;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Gaussien
 
-sig=0.01;
-X = x>(xm-h/2) & x<(xm+h/2);
-Y = y>(yf1-a/2) & y<(yf1+a/2) | y>(yf2-a/2) & y<(yf2+a/2);
+elseif strcmp(type,'Gauss')
 
-G_x = exp( -(x-xm-h/2).^2 / (2*sig^2) ) + exp( -(x-xm+h/2).^2 / (2*sig^2) );
-G_y = exp( -(y-yf1-a/2).^2 / (2*sig^2) ) + exp( -(y-yf1+a/2).^2 / (2*sig^2) )...
-    + exp( -(y-yf2-a/2).^2 / (2*sig^2) ) + exp( -(y-yf2+a/2).^2 / (2*sig^2) );
+    sig=0.01;
+    X = x>(xm-h/2) & x<(xm+h/2);
+    Y = y>(yf1-a/2) & y<(yf1+a/2) | y>(yf2-a/2) & y<(yf2+a/2);
 
-x_i=[xm-h/2 xm+h/2];
-y_j=[yf1-a/2 yf1+a/2 yf2-a/2 yf2+a/2];
-for i=1:2
-    for j=1:4
-        gxy = ( exp( -((x-x_i(i)).^2./(2.*sig.^2)) ) )'* exp(-((y-y_j(j)).^2)./(2.*sig.^2));
-        barr = barr + V .* gxy / max(max(gxy));
+    G_x = exp( -(x-xm-h/2).^2 / (2*sig^2) ) + exp( -(x-xm+h/2).^2 / (2*sig^2) );
+    G_y = exp( -(y-yf1-a/2).^2 / (2*sig^2) ) + exp( -(y-yf1+a/2).^2 / (2*sig^2) )...
+        + exp( -(y-yf2-a/2).^2 / (2*sig^2) ) + exp( -(y-yf2+a/2).^2 / (2*sig^2) );
+
+    x_i=[xm-h/2 xm+h/2];
+    y_j=[yf1-a/2 yf1+a/2 yf2-a/2 yf2+a/2];
+    for i=1:2
+        for j=1:4
+            gxy = ( exp( -((x-x_i(i)).^2./(2.*sig.^2)) ) )'* exp(-((y-y_j(j)).^2)./(2.*sig.^2));
+            barr = barr + V .* gxy / max(max(gxy));
+        end
     end
+
+    barr(~Y,:) = ones(size(barr(~Y,:),1),1) * V * G_x / max(G_x);
+
+    barr(:,X) = ( ones(size(barr(:,X),2),1) * V * G_y / max(G_y) )';
+
+    barr(~Y,X)=V;
+
 end
-
-barr(~Y,:) = ones(size(barr(~Y,:),1),1) * V * G_x / max(G_x);
-
-barr(:,X) = ( ones(size(barr(:,X),2),1) * V * G_y / max(G_y) )';
-
-barr(~Y,X)=V;
-
-
--- Schrodinger_2D/test_pot.m
+++ Schrodinger_2D/test_pot.m
@@ -6,7 +6,7 @@ close all
 x=linspace(0,10,1000);
 y=linspace(0,10,1000);
 
-barr=barr(x,y,1000,5,0.5,4,6,1);
+barr=barr(x,y,1000,5,0.5,4,6,1,'Carre');
 
 figure()
 hold on
