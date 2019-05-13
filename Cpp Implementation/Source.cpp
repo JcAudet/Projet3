37d503f5e67c9e9547949ae15630da03af9aecad
JcAudet <imflyingtoast@hotmail.com>
2019-05-13 18:05:09 -0400

    Cpp start

---
+++ Cpp Implementation/Source.cpp
@@ -0,0 +1,64 @@
+
+#include<algorithm>
+#include<vector>
+#include<boost/math/constants/constants.hpp>
+#include<boost/numeric/ublas/vector.hpp>
+#include<boost/numeric/ublas/io.hpp>
+#include<mgl2/mgl.h>
+
+using namespace boost::numeric::ublas;
+
+boost::numeric::ublas::vector<double> linspace(double sf,double ds) {
+
+	boost::numeric::ublas::vector<double> S(floor(sf / ds));
+	std::generate(S.begin(), S.end(), [ds]() {
+		static double i = 0;
+		return i += ds;
+	});
+
+	return S;
+
+}
+
+int main() {
+
+	// Constantes
+	double hbar = 1.054e-34;
+	double m = 9.019e-31;
+	double PI = boost::math::constants::pi<double>();
+
+	// Discretisation
+	double dx = 5e-12;
+	double xf = 1e-9;
+	double dy = dx;
+	double yf = 1e-9;
+
+	vector<double> X = linspace(xf, dx);
+	vector<double> Y = linspace(yf, dy);
+
+	// Parametres
+	double sig = 8e-11;
+	double lam = 5e-11;
+	vector<double> pos = {1,2};
+	double k = (2 * PI) / lam;
+	std::vector<double> kv(2); kv[0] = k;
+
+	mglData dat(30, 40);
+//	for (long i = 0; i < 30; i++) {
+//		for (long j = 0; j < 40; j++) {
+//			dat[i + 30 * j] = 1;
+//		}
+//	}
+
+
+	mglGraph gr;
+	gr.Light(true);
+
+	gr.Surf(dat);
+
+
+
+
+	system("pause");
+	return 0;
+}
